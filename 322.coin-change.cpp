b33cd2d86efd6161031ffdbbd6e74ccd0fb836dc
unknown <archit.agrawal.003@gmail.com>
2020-07-24 00:42:25 +0530

    DP

---
+++ 322.coin-change.cpp
@@ -0,0 +1,21 @@
+#define  ll long long int 
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int  amount) {
+        
+    vector<int> need(amount+1, amount+1);
+    need[0] = 0;
+    for (int a=1; a<=amount; a++)
+        for (int c : coins)
+            if (c <= a)
+                need[a] = min(need[a], need[a-c] + 1);
+    return need.back() > amount ? -1 : need.back();
+        
+        
+        
+        
+        
+        
+        
+    }
+};
