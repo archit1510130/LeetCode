38601420f2aed5ab2162244662f8f927030ab0ab
archit1510130 <archit.agrawal.003@gmail.com>
2020-08-15 17:56:36 +0530

    DP

---
+++ 617.merge-two-binary-trees.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=617 lang=cpp
+ *
+ * [617] Merge Two Binary Trees
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        
+    }
+};
+// @lc code=end
+
--- 646.maximum-length-of-pair-chain.cpp
+++ 646.maximum-length-of-pair-chain.cpp
@@ -1,15 +1,25 @@
-/*
- * @lc app=leetcode id=646 lang=cpp
- *
- * [646] Maximum Length of Pair Chain
- */
-
-// @lc code=start
 class Solution {
 public:
     int findLongestChain(vector<vector<int>>& pairs) {
         
+        int n=pairs.size();
+        if(n==0)
+            return 0;
+        sort(pairs.begin(),pairs.end());
+        vector<int>dp(n+1,1);
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(pairs[i][0]>pairs[j][1])
+                {
+                    dp[i]=max(1+dp[j],dp[i]);
+                }
+            }
+        }
+        
+        return dp[n-1];
+        
     }
 };
-// @lc code=end
-
