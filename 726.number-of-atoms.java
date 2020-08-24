f44bdf2617f88b971221b53ea97a5b9e8aecb627
archit1510130 <archit.agrawal.003@gmail.com>
2020-08-25 00:13:51 +0530

    Random

---
+++ 726.number-of-atoms.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=726 lang=java
+ *
+ * [726] Number of Atoms
+ */
+
+// @lc code=start
+class Solution {
+    public String countOfAtoms(String formula) {
+        return "";
+        
+    }
+}
+// @lc code=end
+
